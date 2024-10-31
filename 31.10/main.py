a6663247f26801d6120d7016b8c6b68ffdc19327
rotat1ck <rotatick32151@gmail.com>
2024-10-31 16:47:07 +0300

    31.10 classwork

---
+++ 31.10/main.py
@@ -0,0 +1,34 @@
+#1
+sentence = 'Мама мыла раму'
+
+print(list(map(len, sentence.split())))
+
+#2
+sentence = 'Мама мыла раму'
+sentence = sentence.lower()
+
+print(dict(set(map(lambda x: (x, sentence.count(x)), sentence))))
+
+#3
+numbers = [1,2,3,4,5]
+dict = {}
+
+for i in numbers:
+    dict[i] = [j for j in range(1, i + 1) if i % j == 0]
+
+print(dict)
+
+#4
+string = 'Российская Федерация'
+string = string.upper()
+string = string.split()
+
+print(''.join(list(map(lambda x: x[0], string))))
+
+#5
+rle = [('a', 2), ('b', 3), ('c', 1)]
+
+print(''.join(list(map(lambda x : x[0]*x[1], rle))))
+
+
+
