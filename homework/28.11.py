a5ba88c7883fe1c2bbfc61b60bdaadfdf460e703
rotat1ck <rotatick32151@gmail.com>
2024-11-27 16:38:40 +0300

    28.11 homework

---
+++ homework/28.11.py
@@ -0,0 +1,21 @@
+def uniqueFileNames(s: set):
+    result = set()
+    for item in s:
+        if ".jpg" in item:
+            result.add(item)
+            
+    return result
+
+s = ("book_cover.jpg cover.png Book_cover.jpg "
+    "illustration.jpg ILLUSTRATION.JPG my_cover.png "
+    "photo.gif award.jpg Award.jpg award.JPG".lower().strip().split(" "))
+
+print(uniqueFileNames(set(s)))
+
+
+def getColors(amount):
+    namedColors = set(input(f"Введите {i + 1} цвет: ").lower() for i in range(amount))
+    return "Принято" if len(namedColors) == amount else "Повтор"
+    
+
+print(getColors(int(input("Введите кол-во цветов: "))))
